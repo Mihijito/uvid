65497136a9cc2dfa1b3e6138a7c731e60bd84b10
Tristan Ortiz-Duchesne <tristanod@Tristans-MacBook-Pro.local>
2020-05-03 20:03:43 -0400

    Setup environment variables

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import { createServer } from 'http';
 import { ChatRoomServiceImpl } from './chatRoom';
+require('dotenv').config()
 
 const httpServer = createServer();
-const chatRoomService = new ChatRoomServiceImpl(httpServer, '8080');
+const chatRoomService = new ChatRoomServiceImpl(httpServer, process.env.PORT!);
 chatRoomService.openChatRoomServer();
